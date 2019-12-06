77be5dfd04b7c2bc6fa1b82e11966cf74883e3cf
arip1997 <ariefr.santoso@gmail.com>
2019-12-06 10:43:19 +0700

    Memvalidasi Login

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Builder, By, Key, util } = require("selenium-webdriver");
+
+
+async function googling() {
+  //buka browser
+  let driver = await new Builder().forBrowser("chrome").build();
+  // buka url google
+  await driver.get("https://s2.demo.opensourcecms.com/orangehrm/symfony/web/index.php/auth/login");
+  await driver.findElement(By.name("txtUsername")).sendKeys("opensourcecms");
+  await driver.findElement(By.name("txtPassword")).sendKeys("opensourcecms");
+  await driver.findElement(By.name("Submit")).click();
+
+  let text = await driver.findElement(By.xpath("//li")).getText();
+
+}
+
+googling();
